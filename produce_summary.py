71a0fa122c95853a07ee2da726f433f608365488
Gillian Hammett <gillerbiller@gmail.com@hackbrightacademy.com>
2020-04-30 01:31:21 +0000

    output fixed to display number of melons delivered, type of melone, and total cost. Variable names more clearly defined, code cleaned

--- produce_summary.py
+++ produce_summary.py
@@ -1,43 +1,42 @@
+
 print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+daily_delivery_report = open("um-deliveries-20140519.txt")
+for item in daily_delivery_report:
+    item = item.rstrip()
+    words = item.split('|')
 
     melon = words[0]
-    count = words[0]
-    amount = words[0]
+    count = words[1] 
+    amount = words[2]
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+    print(f"Delivered {count} {melon}s for a total of ${amount}.")
+daily_delivery_report.close()
 
 
 print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+daily_delivery_report = open("um-deliveries-20140520.txt")
+for item in daily_delivery_report:
+    item = item.rstrip()
+    words = item.split('|')
 
     melon = words[0]
-    count = words[0]
-    amount = words[0]
+    count = words[1]
+    amount = words[2]
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+    print(f"Delivered {count} {melon}s for a total of ${amount}.")
+daily_delivery_report.close()
 
 
 print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+daily_delivery_report = open("um-deliveries-20140521.txt")
+for item in daily_delivery_report:
+    item = item.rstrip()
+    words = item.split('|')
 
     melon = words[0]
-    count = words[0]
-    amount = words[0]
+    count = words[1]
+    amount = words[2]
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+    print(f"Delivered {count} {melon}s for a total of ${amount}.")
+daily_delivery_report.close()
+
